6ae30b0a71f57b49102a8e9eba01cf61f8acc6b2
Jucel Dorante <juceld@gmail.com>
2024-11-12 20:47:43 -0400

    separate to another func

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,26 @@ use std::{collections::HashMap, fs, path::Path, io::{self, BufRead, BufReader}};
 use html_template_engine::*;
 
 
+fn get_file_content(path: &Path) -> Vec<String> {
+    let mut html_content = Vec::new();
+
+    for line in fs::read_to_string(path).unwrap().lines() {
+        html_content.push(line.to_string())
+    }
+
+    html_content
+}
 
 fn main() {
     let mut context = HashMap::new();
 
     context.insert("name".to_string(), "Bob".to_string());
-
     context.insert("city".to_string(), "Caracas".to_string());
 
-
     // Reading file:
-    let mut html_content = Vec::new();
-
     let path = Path::new("/home/jucester/Work/Personal/TheOwlymath/Rust/RustPath/Level01/html-template-engine/src/test.html");
-    for line in fs::read_to_string(path).unwrap().lines() {
-        html_content.push(line.to_string())
-    }
+    let html_content = get_file_content(path);
 
-
-    // for line in io::stdin().lock().lines() {
     for line in html_content {
         match get_content_type(&line.clone()) {
             ContentType::TemplateVariable(content) => {
