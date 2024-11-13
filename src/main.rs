a3b83125e50e79604a5d6901a7c5fa13f312e392
Jucel Dorante <juceld@gmail.com>
2024-11-12 21:15:32 -0400

    First generation of parsed file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::{collections::HashMap, fs, path::Path, io::{self, BufRead, BufReader}};
 use html_template_engine::*;
+use std::{
+    collections::HashMap,
+    io::BufReader,
+    path::Path,
+};
 
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
 
 fn get_file_content(path: &Path) -> Vec<String> {
     let mut html_content = Vec::new();
@@ -19,19 +27,36 @@ fn main() {
     context.insert("city".to_string(), "Caracas".to_string());
 
     // Reading file:
-    let path = Path::new("/home/jucester/Work/Personal/TheOwlymath/Rust/RustPath/Level01/html-template-engine/src/test.html");
+    let path: &Path = Path::new("/home/jucester/Work/Personal/TheOwlymath/Rust/RustPath/Level01/html-template-engine/src/test.html");
     let html_content = get_file_content(path);
+
+    let mut html_content_parsed = Vec::new();
 
     for line in html_content {
         match get_content_type(&line.clone()) {
             ContentType::TemplateVariable(content) => {
                 let html = generate_html(content, context.clone());
-                println!("Content");
+                html_content_parsed.push(html);
             }
-            ContentType::Literal(text) => println!("Literal"),
+            ContentType::Literal(text) => {
+                html_content_parsed.push(text);
+            }
             ContentType::Tag(TagType::ForTag) => println!("FOR TAG"),
             ContentType::Tag(TagType::IfTag) => println!("IF TAG"),
-            ContentType::Unrecognized => println!("UNRECOGNIZED")
+            ContentType::Unrecognized => println!("UNRECOGNIZED"),
         }
     }
+
+    let mut html_bytes: Vec<u8> = Vec::new();
+    
+    for line in html_content_parsed{
+        html_bytes.extend(line.as_bytes());
+        html_bytes.push(b'\n');
+    }
+
+    let new_file: &Path = Path::new("/home/jucester/Work/Personal/TheOwlymath/Rust/RustPath/Level01/html-template-engine/src/new_file.html");
+
+    let mut f = fs::File::create(new_file).unwrap();
+
+    f.write_all(&html_bytes);
 }
